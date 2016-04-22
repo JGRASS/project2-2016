0203967a443bea290288ee04f444c77656e95048
AleksaDencic <alekdencic@hotmail.com>
2016-04-23 01:25:55 +0200

    [FEATURE] Add classes in the package model
    Add classes for Test, EntranceExam, Candidate and Serialization

---
+++ src/exam/scanner/model/test/EntranceExam.java
@@ -0,0 +1,39 @@
+package exam.scanner.model.test;
+
+public class EntranceExam {
+
+	String candidatePassword;
+	String examPassword;
+	char[] answers = new char[20];
+	
+	public EntranceExam(String candidatePassword, String examPassword, char[] answers) {
+		super();
+		this.setCandidatePassword(candidatePassword);
+		this.setExamPassword(examPassword); 
+		this.setAnswers(answers);
+	}
+
+	public String getCandidatePassword() {
+		return candidatePassword;
+	}
+
+	public void setCandidatePassword(String candidatePassword) {
+		this.candidatePassword = candidatePassword;
+	}
+
+	public String getExamPassword() {
+		return examPassword;
+	}
+
+	public void setExamPassword(String examPassword) {
+		this.examPassword = examPassword;
+	}
+
+	public char[] getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(char[] answers) {
+		this.answers = answers;
+	}
+}
---
+++ src/exam/scanner/model/test/Test.java
@@ -0,0 +1,22 @@
+package exam.scanner.model.test;
+
+public class Test {
+	
+	private int testPassword;
+	private char[] answers = new char[20];
+	
+	public int getTestPassword() {
+		return testPassword;
+	}
+	public void setTestPassword(int testPassword) {
+		this.testPassword = testPassword;
+	}
+	public char[] getAnswers() {
+		return answers;
+	}
+	public void setAnswers(char[] answers) {
+		this.answers = answers;
+	}
+	
+	
+}
