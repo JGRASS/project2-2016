42c2acd9941624ce04245bd41bba87e089297945
AleksaDencic <alekdencic@hotmail.com>
2016-04-23 01:25:55 +0200

    [FEATURE] Add classes for test, exam, candidate and serialization

---
+++ src/exam/scanner/model/test/Candidate.java
@@ -0,0 +1,33 @@
+package exam.scanner.model.test;
+
+public class Candidate {
+
+	private String candidatePassword;
+	private String examPassword;
+	private int results;
+	
+	public String getCandidatePassword() {
+		return candidatePassword;
+	}
+	public void setCandidatePassword(String candidatePassword) {
+		this.candidatePassword = candidatePassword;
+	}
+	public String getExamPassword() {
+		return examPassword;
+	}
+	public void setExamPassword(String examPassword) {
+		this.examPassword = examPassword;
+	}
+	public int getResults() {
+		return results;
+	}
+	public void setResults(int results) {
+		this.results = results;
+	}
+	
+	
+	
+	
+	
+	
+}
