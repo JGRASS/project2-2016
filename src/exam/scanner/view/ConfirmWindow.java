795a0ba32b9feeb10cf3844c8421c7416f114a68
davidoviclazar <ld170.14@gmail.com>
2016-04-22 12:45:17 +0200

    [BUGFIX] Correct the problems caused by rerouting

--- src/exam/scanner/view/ConfirmWindow.java
+++ src/exam/scanner/view/ConfirmWindow.java
@@ -1,4 +1,4 @@
-package view;
+package exam.scanner.view;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
--- src/exam/scanner/view/MainAppWindow.java
+++ src/exam/scanner/view/MainAppWindow.java
@@ -1,4 +1,4 @@
-package view;
+package exam.scanner.view;
 
 import java.awt.EventQueue;
 
